c88b48450f34dfccb843dc4e5d4df1469521eba7
harishanand51 <harishanand51@gmail.com>
2025-01-19 19:57:42 -0500

    prompt engineering

---
+++ springai-prompt-engineering/src/test/java/com/springframework/springaipromptengineering/SpringaiPromptEngineeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.springaipromptengineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiPromptEngineeringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
